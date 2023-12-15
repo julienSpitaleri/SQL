fdf8b5115481b1a55c13e2414ca0354e22fb7de3
Navarant <anthony.navarro@viacesi.fr>
2023-12-15 12:00:19 +0100

    Update Exercice.sql

--- Exercice.sql
+++ Exercice.sql
@@ -1,6 +1,9 @@
 SELECT * FROM `client` WHERE prenom ='muriel' AND password =SHA1('test11');
+
 SELECT nom from commande_ligne GROUP BY nom HAVING COUNT(nom) > 1
+
 SELECT nom, commande_id from commande_ligne GROUP BY nom HAVING COUNT(nom) > 1
+
 UPDATE commande_ligne SET prix_total = (prix_unitaire*quantite)
 
 SELECT prix_total, date_achat, client.nom, client.prenom
