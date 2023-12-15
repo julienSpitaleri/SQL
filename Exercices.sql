7294d5962c8351f35f631794f7f219c152a7ff91
Eliott <eliottcheminal@gmail.com>
2023-12-15 11:38:35 +0100

    EX5

--- Exercices.sql
+++ Exercices.sql
@@ -7,3 +7,10 @@ select nom , commande_id from commande_ligne group by nom having count(nom) > 1;
 
 UPDATE commande_ligne 
 SET prix_total = (prix_unitaire) * quantite;
+
+SELECT prix_total from commande_ligne 
+JOIN (SELECT prenom , nom from client) AS clients;
+
+SELECT prix_total,date_achat,client.prenom, client.nom from commande_ligne
+INNER JOIN client ON client.id = commande_ligne.id 
+INNER JOIN commande ON commande.id = commande_ligne.id;
