44290ab9bdc0c41d635a3eb80f1f6ffcea61b9eb
Hanan Rebaia <hanan.rebaia@viacesi.fr>
2023-12-15 11:56:53 +0100

    ✨add instruction and answers in sql file

--- exercice1.sql
+++ exercice1.sql
@@ -1,22 +1,30 @@
-Q1
+Q1 : Obtenir l’utilisateur ayant le prénom “Muriel” et le mot de passe “test11”, 
+sachant que l’encodage du mot de passe est effectué avec l’algorithme Sha1.
+SELECT * FROM `client` WHERE prenom = 'Muriel';
+
+
 SELECT * FROM `client`
 WHERE prenom = 'Muriel'
 AND password = SHA1('test11');
 
-Q2
+
+Q2 : Obtenir la liste de tous les produits qui sont présent sur plusieurs commandes.
+
 SELECT nom, COUNT(*) AS nbr_items 
 FROM `commande_ligne` 
 GROUP BY nom 
 HAVING nbr_items > 1
 ORDER BY nbr_items DESC
 
-Q3
+Q3 : Obtenir la liste de tous les produits qui sont présent sur plusieurs 
+commandes et y ajouter une colonne qui liste les identifiants des commandes associées.
+
+SELECT nom, COUNT(*) AS nbr_items , GROUP_CONCAT(`commande_id`) AS liste_commandes
+FROM `commande_ligne` 
+GROUP BY nom 
+HAVING nbr_items > 1
+ORDER BY nbr_items DESC
+
+Q4 : Enregistrer le prix total à l’intérieur de chaque ligne des commandes, en fonction du prix unitaire et de la quantité
 UPDATE `commande_ligne` 
 SET  `prix_total` = (`quantite` * `prix_unitaire`)
-
-Q4
-SELECT client.prenom, client.nom, commande.date_achat, commande_id, SUM(prix_total) AS prix_commande 
-FROM `commande_ligne` 
-LEFT JOIN commande ON commande.id = commande_ligne.commande_id
-LEFT JOIN client ON client.id = commande.client_id
-GROUP BY `commande_id`
