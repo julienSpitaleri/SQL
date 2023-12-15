7c0d8c35ffa28f355851e0866a0af94517fd0349
Navarant <anthony.navarro@viacesi.fr>
2023-12-15 11:59:38 +0100

    Update Exercice.sql

--- Exercice.sql
+++ Exercice.sql
@@ -9,3 +9,5 @@ INNER JOIN commande
 ON client.id = commande.id
 INNER JOIN commande_ligne
 ON client.id = commande_ligne.id
+
+UPDATE commande SET cache_prix_total = (SELECT prix_total FROM commande_ligne WHERE id = commande.id)
