21b41318af4e4e47db29a9ef8ba3deb16e618fbd
Navarant <anthony.navarro@viacesi.fr>
2023-12-15 14:23:06 +0100

    Update Exercice.sql

--- Exercice.sql
+++ Exercice.sql
@@ -13,9 +13,17 @@ ON client.id = commande.id
 INNER JOIN commande_ligne
 ON client.id = commande_ligne.id
 
-UPDATE commande SET cache_prix_total = (SELECT prix_total FROM commande_ligne WHERE id = commande.id)
+UPDATE commande SET cache_prix_total = (SELECT SUM(prix_total) FROM commande_ligne WHERE commande.id = commande_id);
 
 SELECT MONTH(date_achat) as 'Mois',SUM(cache_prix_total) as 'prix mensuel'
 FROM commande
 GROUP By Month(date_achat)
 ORDER BY Month(date_achat)
+
+SELECT nom,prenom, client.id, sum(commande.cache_prix_total) 
+FROM client 
+LEFT JOIN commande 
+ON client.id = client_id 
+group by client_id 
+ORDER by sum(commande.cache_prix_total) DESC 
+LIMIT 10
