a849c4d009f8c13d76c0503411d13d60bb5dd9c8
DoubleRiichi <DoubleRiichi>
2023-12-15 10:53:57 +0100

    ajout exercise 5

--- exercices.sql
+++ exercices.sql
@@ -2,6 +2,7 @@
 2)  SELECT nom FROM commande_ligne group by nom having count(*) > 1;
 3)  SELECT id, nom FROM commande_ligne group by nom having count(*) > 1;
 4)  UPDATE commande_ligne SET prix_total = prix_unitaire * quantite;
+
 5)  SELECT commande_ligne.prix_total, commande.date_achat, client.nom, client.prenom
     FROM commande_ligne 
     INNER JOIN commande
@@ -9,4 +10,4 @@
     INNER JOIN client
     ON commande.id = client.id;
 
-6) 
+6)  UPDATE commande SET prix_total_cache 
