45ac458b93939fb99e028e8344447707353e2ae9
Eliott <eliottcheminal@gmail.com>
2023-12-15 10:27:43 +0100

    ex3

--- Exercices.sql
+++ Exercices.sql
@@ -1,3 +1,6 @@
 SELECT * FROM `client` WHERE prenom = 'Muriel' AND password = SHA1('test11');
 
 select nom from commande_ligne group by nom having count(nom) > 1;
+
+
+select nom , commande_id from commande_ligne group by nom having count(nom) > 1;
