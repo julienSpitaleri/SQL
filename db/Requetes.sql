42aab905148b42e084aa1447533d180584dc489e
Norlik13 <louis.garagnon@viacesi.fr>
2023-12-15 12:08:57 +0100

    Consigne 1-7

--- db/Requetes.sql
+++ db/Requetes.sql
@@ -14,4 +14,11 @@ UPDATE `commande_ligne` SET `prix_total` = `prix_unitaire` * `quantite`;
 SELECT client.prenom, client.nom, commande_id, commande.date_achat, prix_total FROM `commande_ligne` LEFT JOIN commande ON commande.id = commande_ligne.commande_id LEFT JOIN client ON client.id = commande.client_id GROUP BY `commande_id`;
 
 --Consigne 6
+UPDATE `commande` INNER JOIN (SELECT commande_id, SUM(commande_ligne.prix_total) AS somme FROM commande_ligne GROUP BY commande_id) tmp_table ON commande.id = tmp_table.commande_id SET commande.cache_prix_total = tmp_table.somme;
 
+UPDATE `commande` SET cache_prix_total = (SELECT SUM(prix_total) FROM commande_ligne WHERE commande.id = commande_id);
+
+--Consigne 7
+SELECT YEAR(date_achat), MONTH(date_achat), SUM(cache_prix_total) FROM `commande` GROUP BY YEAR(date_achat), MONTH(date_achat)
+
+--Consigne 8
