d9ddc15198de1c4214805c76cb9caf1ca635752f
Eliott <eliottcheminal@gmail.com>
2023-12-15 14:58:55 +0100

    EX11

--- Exercices.sql
+++ Exercices.sql
@@ -43,5 +43,14 @@ ORDER by date_achat;
 ALTER TABLE commande
 ADD category NUMERIC;
 
+UPDATE commande
+SET category = (
+CASE cache_prix_total
+       WHEN cache_prix_total>200 THEN 1
+       WHEN cache_prix_total<500 THEN 2
+       WHEN cache_prix_total<1000 THEN 3
+       ELSE 4
+END);
+
 
 
