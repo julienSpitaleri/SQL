b2b13f31888126512d203c27959e4039cf497d56
Norlik13 <louis.garagnon@viacesi.fr>
2023-12-15 11:10:24 +0100

    Consigne 1-5

--- db/Requetes.sql
+++ db/Requetes.sql
@@ -2,3 +2,16 @@
 SELECT `prenom`, `password` FROM `client` WHERE `prenom`="Muriel" and `password`=SHA1('test11');
 
 --Consigne 2
+SELECT nom, COUNT(*) AS nbr_items FROM `commande_ligne` GROUP BY nom HAVING nbr_items > 1;
+
+--Consigne 3
+SELECT nom, COUNT(*) AS nbr_items , GROUP_CONCAT(`commande_id`) AS ligne_commande FROM `commande_ligne` GROUP BY nom HAVING nbr_items > 1;
+
+--Consigne 4
+UPDATE `commande_ligne` SET `prix_total` = `prix_unitaire` * `quantite`;
+
+--Consigne 5
+SELECT client.prenom, client.nom, commande_id, commande.date_achat, prix_total FROM `commande_ligne` LEFT JOIN commande ON commande.id = commande_ligne.commande_id LEFT JOIN client ON client.id = commande.client_id GROUP BY `commande_id`;
+
+--Consigne 6
+
