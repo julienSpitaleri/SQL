2a9d6d19c021f0c46bf26dafd9e6070eb813ca97
DoubleRiichi <DoubleRiichi>
2023-12-15 14:33:07 +0100

    dernier exercice

--- exercices.sql
+++ exercices.sql
@@ -19,3 +19,28 @@
     ON client.id = commande.client_id
     ORDER BY cache_prix_total DESC LIMIT 10;
     
+9)  SELECT SUM(cache_prix_total), date_achat FROM commande GROUP BY date_achat;
+
+10) ALTER TABLE commande
+    ADD category int;
+
+11) UPDATE commande 
+     SET category = (CASE 
+                        WHEN cache_prix_total < 200 THEN "1"
+                        WHEN cache_prix_total >= 200 AND cache_prix_total <= 500 THEN "2"
+                        WHEN cache_prix_total >= 500 AND cache_prix_total <= 1000 THEN "3"
+                        WHEN cache_prix_total >= 1000 THEN "4"
+                        END);
+
+12) CREATE TABLE commande_category (
+    id int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    category VARCHAR(200) NOT NULL,
+    PRIMARY KEY(id));
+    
+13) INSERT INTO commande_category (category) VALUES
+        ('“1” pour les commandes de moins de 200€'),
+        ('“2” pour les commandes entre 200€ et 500€'),
+        ('“3” pour les commandes entre 500€ et 1.000€'),
+        ('“4” pour les commandes supérieures à 1.000€');
+
+14) DELETE FROM commande WHERE DATE(date_achat) < DATE("2019-02-01");
